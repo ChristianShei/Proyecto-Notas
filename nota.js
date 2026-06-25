84945e54139a08f2b0eed470e554fedca097d536
christian <christianbigchange@gmail.com>
2026-06-25 18:43:54 -0300

    actualizacion

--- nota.js
+++ nota.js
@@ -6,6 +6,7 @@ const listado1 = document.getElementById("lista")
 const listado2 = document.getElementById("listaAlgun")
 
 
+let tareas = JSON.parse (localStorage.getItem("tareas"))|| []
 
 function agregarTarea(){
 
@@ -16,43 +17,65 @@ if (input.value.trim() === ""){
 // crear lista
 const li = document.createElement("li")
 const texto = input.value
-li.textContent = texto
-listado1.appendChild(li)
+    li.textContent = texto
+    listado1.appendChild(li)
+    input.value = ""
 
  // agrega boton para borrar lista
 const borrar = document.createElement("button")
- borrar.textContent = "borrar"
- borrar.Id = "botonera"
- borrar.className = "borrar"
-li.appendChild(borrar)
- borrar.onclick = function () {
+    borrar.textContent = "borrar"
+    borrar.className = "borrar"
+    li.appendChild(borrar)
+    borrar.onclick = function () {
     li.remove();
+     guardarTareas()
 ;}
-
- input.value = ""
+ guardarTareas()
+ 
     
 }
-
-function agregarTareaAlgun(){
-if(input02.value.trim() === ""){
-    return
-};
-
-    const list = document.createElement("li")
-    const text1 = input02.value
-
-    list.textContent = text1
-    listado2.appendChild(list)
-
-    input02.value = ""
-
-document.createAttribute("button")
-
-}
-document.addEventListener("keydown", presionarBoton)
-
+input.addEventListener("keydown", presionarBoton)
 function presionarBoton(presionar){
     if(presionar.key == "Enter"){
         agregarTarea()
     }
 }
+// funcion de boton de tarea de algun momento////////////////////////////////////
+function agregarTareaAlgun(){
+
+    if(input02.value.trim() === ""){
+    return
+    }
+//creación de lista
+const list = document.createElement("li")
+const text1 = input02.value
+        list.textContent = text1
+        listado2.appendChild(list)
+        input02.value = ""
+// boton de borrar
+    const botonera = document.createElement("button")
+        botonera.textContent = "borrar"
+        botonera.className = "borrares"
+        list.appendChild(botonera)
+        botonera.onclick = function(){
+            list.remove()}
+            
+}
+input02.addEventListener("keydown", presionarBotonAlgun)
+function presionarBotonAlgun(presionar){
+    if(presionar.key == "Enter"){
+       agregarTareaAlgun()
+    }
+}
+
+// Cada vez que agregues o elimines una tarea, llamá a una función
+function guardarTareas() {
+    localStorage.setItem("tarea", listado1.innerHTML);
+    localStorage.setItem("tareaAlgunMomento", listado2.innerHTML);
+}
+
+
+window.onload = function () {
+    listado1.innerHTML = localStorage.getItem("tarea") || "";
+    listado2.innerHTML = localStorage.getItem("tareaAlgunMomento") || "";
+};
